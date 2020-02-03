b2f799a56fe56da302ada208be07b781a60aa43c
Bob Forgey <sesamemucho@gmail.com>
2020-02-02 21:30:55 -0800

    Using org-plus-contrib,sort-of

--- init.el
+++ init.el
@@ -9,6 +9,8 @@
 ;;
 
 ;;; Code:
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file t)
 (prefer-coding-system 'utf-8)
 ;; (unless (boundp 'package-user-dir)
 ;;   (unless (boundp 'package-archive-contents)
@@ -18,14 +20,13 @@
 ;;     (package-install (elt (cdr (assoc 'org-plus-contrib package-archive-contents)) 0))
 ;;     (package-install (elt (cdr (assoc 'use-package package-archive-contents)) 0))))
 ;; (assoc-delete-all 'org package--builtins)
-(add-to-list 'load-path "/home/bob/Projects/src/emacs/emacs_config_tyson/startup/use-package")
-(add-to-list 'load-path "/home/bob/Projects/src/emacs/emacs_config_tyson/startup/bind-key")
+(add-to-list 'load-path (expand-file-name "startup/use-package" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "startup/bind-key" user-emacs-directory))
 (require 'use-package)
 (setq use-package-always-ensure nil)
 (use-package org)
 ;;(defcustom ssmm/cfg-file (concat user-emacs-directory "config")
-;;(setq ssmm/cfg-dir "/home/bob/.emacs.d/")
-(setq ssmm/cfg-dir "/home/bob/Projects/src/emacs/emacs_config_tyson/")
+(setq ssmm/cfg-dir "/home/bob/.emacs.d/")
 (setq ssmm/cfg-file (concat ssmm/cfg-dir "config"))
 ;;  "The base name for the .org file to use for Emacs initialization.")
 (when (file-newer-than-file-p (concat ssmm/cfg-file ".org") (concat ssmm/cfg-file ".el"))
